18d8c3ac2a87aca72501129076383907d0f2e1ef
patybn3 <paty.nigro@gmail.com>
2022-10-13 22:29:05 -0400

    ArrayList game of cards done

--- src/Card.java
+++ src/Card.java
@@ -3,7 +3,7 @@ public class Card
 {
     // I read this source https://fpl.cs.depaul.edu/jriely/thinkapjava/thinkapjava.array2.html
     public static final String[] SUITS = {"CLUBS", "DIAMONDS", "HEARTS", "SPADES"};
-    public static final String[] FACES = {"ACE", "2", "3", "4", "5", "6", "7", "8", "9", "10", "JACK", "QUEEN", "KING"};
+    public static final String[] FACES = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "JACK", "QUEEN", "KING", "ACE"};
 
     private int suit;
     private int face;
